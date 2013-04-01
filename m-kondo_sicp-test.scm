9fb63cef226e032ed3747fab28ba912bc6e75803
johnsmith0707 <misamisakondo@gmail.com>
2013-04-01 11:35:01 +0900

    1.1.6までの実装とテストを書いた.i
    test endを追記

--- m-kondo_sicp-test.scm
+++ m-kondo_sicp-test.scm
@@ -12,3 +12,12 @@
 (test* "square 9" 81 (square 9))
 (test* "sum-of-squares 1 2" 5 (sum-of-squares 1 2))
 (test* "sum-of-squares 2 3" 13 (sum-of-squares 2 3))
+(test* "f 1" 8 (f 1))
+(test* "f 2" 25 (f 2))
+
+(test-section "# 1.1.6")
+(test* "abs 1" 1 (abs 1))
+(test* "abs 0" 0 (abs 0))
+(test* "abs -1" 1 (abs -1))
+
+(test-end)
